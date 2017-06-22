ab108153cf2ff85e8f951054d9710c06b51e60e9
Joe Dooley <hello@developingdesigns.com>
2017-06-22 16:36:25 -0400

    added donate links to the footer

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -1,7 +1,11 @@
 <template>
 	<v-footer>
-		<div class="text-xs-center">
+		<div class="copyright">
 			<p>©2017 <a href="/" title="CoinCaps.io - Cryptocurrency Market Caps">CoinCaps.io</a> - All Rights Reserved</p>
+		</div>
+		<div class="donations">
+			<p>Donate BTC: 1FQTPnqAPgqCZD8YpLj45Bo5f97hf8zauE</p>
+			<p>Donate ETH: 0x16cfdDA0671D9C17B12F99f062c8e8D58Eb69939</p>
 		</div>
 	</v-footer>
 </template>
@@ -16,10 +20,22 @@
 
 <style lang="scss" scoped>
 	.footer {
+		height: auto;
+		justify-content: space-between;
+
+		@media (max-width: 768px) {
+			flex-direction: column;
+		}
+
+		.copyright {
+			margin-bottom: 10px;
+			margin-top: 10px;
+		}
+
 		a {
 			color: #fff;
 		}
-		
+
 		p {
 			margin-bottom: 0;
 		}
